9c851fb41e2dd01608efc3defded3d5d9621af9a
Jace Babin <jbabin91@gmail.com>
2026-02-17 19:28:04 -0600

    chore: add nitro for node-server deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
@@ -15,6 +16,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
     paraglideVitePlugin({
       cookieName: 'APP_LOCALE',
